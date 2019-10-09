f36cc62302281225bc1ffbef933496f021cd78ce
Daniel517 <ramirezdaniel51@yahoo.com>
2019-10-08 21:10:42 -0700

    Created ios project.

---
+++ PasswordiOS/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  PasswordiOS
+//
+//  Created by Daniel Ramirez on 10/8/19.
+//  Copyright © 2019 Daniel Ramirez. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("LoginView")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
